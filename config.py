a6503e4d351195d8253c375d8243f590733e9469
ruishili <1050731899@qq.com>
2019-05-05 16:32:49 +0800

    ok

---
+++ config.py
@@ -0,0 +1,8 @@
+MONGO_URL = '192.168.179.135'
+MONGO_DB = 'tiaobao'
+MONGO_TABLE = 'product'
+
+SERVICE_ARGS = ['--load-images=false','--disk-cache=true']
+EXECUTETABLE_PATH = r'‪E:/ProgramFiles/phantomjs-2.1.1-windows/bin/phantomjs.exe'
+KEYWORD = '美食'
+
